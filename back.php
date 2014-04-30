e53920f967e6acdcc453c164ee637266d2180320
Alexar <alexarpen@gmail.com>
2014-04-30 14:40:44 +1000

    Separating parts into files.

---
+++ back.php
@@ -0,0 +1,36 @@
+	<section>
+		<div class="smily">(:</div>
+		<div class="code">
+			<div class="row row-bold">
+				@media mobile first {
+			</div>
+			<div class="row">
+				HOOK_drupal_alter
+			</div>
+			<div class="row">
+				&lt;?php $('developer');
+			</div>
+			<div class="row">
+				love@linux:~# _
+			</div>
+			<div class="row">
+				(nojQuery($){})(false);
+			</div>
+			<div class="row">
+				&lt;![CDATA[ .. ]]&gt;
+			</div>
+			<div class="row">
+				sass --help
+			</div>
+			<div class="row row-bold">
+				}
+			</div>
+			<div class="copyright">
+				@alexarpen CC BY-SA &copy;2014
+				<!-- https://api.github.com/repos/alexarpen/vcard/tarball/webdeveloper -->
+			</div>
+			<div class="version">
+				v1.0
+			</div>
+		</div>
+	</section>
---
+++ front.php
@@ -0,0 +1,18 @@
+	<section>
+		<div class="x">X?</div>
+		<div class="content social">
+			@alexarpen
+		</div>
+		<div class="content info">
+			<h1>Alexar Pendashteh</h1>
+			<div class="number">
+				+61 410 7220 54
+			</div>
+			<div class="email">
+				hi@alexar.me
+			</div>
+		</div>
+		<div class="git">
+			$ git clone git@github.com:alexarpen/vcard.git
+		</div>
+	</section>
