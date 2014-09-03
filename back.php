3717dc1ed0ead39ff5e947d6cb88ea86226b59e3
Alexar <alexarpen@gmail.com>
2014-09-03 22:29:50 +1000

    Removing data from back and replacing by a smily

--- back.php
+++ back.php
@@ -1,36 +1,10 @@
 	<section>
-		<div class="x">X?</div>
-		<div class="code">
-			<div class="row row-bold">
-				@media mobile first {
-			</div>
-			<div class="row">
-				HOOK_drupal_alter
-			</div>
-			<div class="row">
-				&lt;?php `developer`;
-			</div>
-			<div class="row">
-				love@linux:~# _
-			</div>
-			<div class="row">
-				(nojQuery($){})(false);
-			</div>
-			<div class="row">
-				&lt;![CDATA[ Semantic Web ]]&gt;
-			</div>
-			<div class="row">
-				sass --help
-			</div>
-			<div class="row row-bold">
-				}
-			</div>
-			<div class="copyright">
-				@alexarpen CC BY-SA &copy;2014
-				<!-- https://api.github.com/repos/alexarpen/vcard/tarball/webdeveloper -->
-			</div>
-			<div class="version">
-				v1.0
-			</div>
+		<div class="smily">:)</div>
+		<div class="copyright">
+			@alexarpen CC BY-SA &copy;2014
+			<!-- https://api.github.com/repos/alexarpen/vcard/tarball/webdeveloper -->
+		</div>
+		<div class="version">
+			v1.0
 		</div>
 	</section>
